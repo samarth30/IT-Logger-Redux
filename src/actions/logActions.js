2cefb609c498cae185d573ed987ff9b7abe23d39
samarth <samarth.gugnani30@gmail.com>
2020-05-23 21:25:58 +0530

    added

---
+++ src/actions/logActions.js
@@ -0,0 +1,159 @@
+import {
+  GET_LOGS,
+  SET_LOADING,
+  LOGS_ERROR,
+  ADD_LOG,
+  DELETE_LOG,
+  SET_CURRENT,
+  UPDATE_LOG,
+  CLEAR_CURRENT,
+  SEARCH_LOGS,
+} from "./types";
+
+// export const getLogs = () =>{
+//   return async (dispatch) =>{
+//      setLoading();
+
+//      const res = await fetch('/logs');
+//      const data = await res.json();
+
+//      dispatch({
+//          type:GET_LOGS,
+//          payload:data
+//      })
+//   }
+// }
+
+// get logs from server
+export const getLogs = () => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch("http://localhost:5000/logs");
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
+
+// add a new log
+export const addLog = (log) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch("http://localhost:5000/logs", {
+      method: "POST",
+      body: JSON.stringify(log),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOG,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
+
+// delete log from server
+export const deleteLog = (id) => async (dispatch) => {
+  try {
+    setLoading();
+
+    await fetch(`http://localhost:5000/logs/${id}`, {
+      method: "DELETE",
+    });
+
+    dispatch({
+      type: DELETE_LOG,
+      payload: id,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
+
+// update log from server
+export const updateLog = (log) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch(`http://localhost:5000/logs/${log.id}`, {
+      method: "PUT",
+      body: JSON.stringify(log),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    const data = await res.json();
+
+    dispatch({
+      type: UPDATE_LOG,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
+
+// searcg logs from server
+export const searchLogs = (text) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch(`http://localhost:5000/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data,
+    });
+  }
+};
+
+// set current log
+export const setCurrent = (log) => {
+  return {
+    type: SET_CURRENT,
+    payload: log,
+  };
+};
+
+//clear current log
+export const clearCurrent = () => {
+  return {
+    type: CLEAR_CURRENT,
+    payload: null,
+  };
+};
+
+// set loading to true
+export const setLoading = () => {
+  return { type: SET_LOADING };
+};
